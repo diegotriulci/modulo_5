721dd291654d6bca011b52d38ebe8ef5a0123480
diegotriulci <diego.triulci@outlook.com>
2026-04-28 20:04:07 -0300

    subindo arquivos de concatenacao

---
+++ Concatena_variaveis.py
@@ -0,0 +1,6 @@
+#variaveis
+nome = input("Digite seu nome \n")
+idade = input("Digite sua idade \n")
+print ("Seu nome é ", nome," e você tem " , idade, " de idade")
+convert_idade = (int(idade))
+print ("Daqui a 5 anos, você terá ", convert_idade + 5, "anos")
